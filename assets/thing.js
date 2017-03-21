3676f18bb20f4eb06952f6297db131d90a29a5ad
Matt <mfyke@live.com>
2017-03-21 13:42:31 -0700

    all functions but no readme

--- assets/thing.js
+++ assets/thing.js
@@ -9,25 +9,23 @@ var config = {
 
 var database = firebase.database();
 
-var employeeName = "";
-var role = "";
-var startDate = "";
-var monthsWorked = 0;
-var monthlyRate = 0;
-var totalBilled= 0;
+var startTime;
+var currentTime;
+var frequency;
+
 
 $("#bonerFucker").on("click", function(event){
 	event.preventDefault();
 	name=$("#name-input").val().trim();
-	role=$("#role-input").val().trim();
-	startDate=$("#start-input").val().trim();
-	monthlyRate=$("#rate-input").val().trim();
+	destination=$("#destination-input").val().trim();
+	firstTrainTime=$("#start-input").val().trim();
+	frequency=$("#frequency-input").val().trim();
 
 	database.ref().push({
-		name: name,
-		role: role,
-		start: startDate,
-		rate: monthlyRate
+		trainName: name,
+		destination: destination,
+		firstTrainTime: firstTrainTime,
+		frequency: frequency
 	});
 
 	$("#name-input").val("");
@@ -36,18 +34,38 @@ $("#bonerFucker").on("click", function(event){
 	$("#rate-input").val("");
 });
 database.ref().on("child_added", function(snapshot){
+	var tFrequency = snapshot.val().frequency;
+
+    var firstTime = snapshot.val().firstTrainTime;
+
+    var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
 	var row = $("<tr>");
-	var col1= $("<td>" + snapshot.val().name + "</td>");
-	var col2= $("<td>" + snapshot.val().role + "</td>");
-	var col3= $("<td>" + snapshot.val().start + "</td>");
-	var col4= $("<td>");
-	var col5= $("<td>" + snapshot.val().rate + "</td>");
-	var col6= $("<td>");
+	var col1= $("<td>" + snapshot.val().trainName + "</td>");
+	var col2= $("<td>" + snapshot.val().destination + "</td>");
+	var col3= $("<td>" + snapshot.val().frequency + "</td>");
+	var col4= $("<td>" + moment(nextTrain).format("hh:mm") + "</td>");
+	var col5= $("<td>" +  tMinutesTillTrain + "</td>");
 	$(col1).appendTo(row);
 	$(col2).appendTo(row);
 	$(col3).appendTo(row);
 	$(col4).appendTo(row);
 	$(col5).appendTo(row);
-	$(col6).appendTo(row);
 	$("#employeeArea").append(row);
 });
